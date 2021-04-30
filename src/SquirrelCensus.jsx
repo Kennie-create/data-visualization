0e2dd2797b1d41c624978325683b4129681d3608
kennie-create <kennieboulin1989@gmail.com>
2021-04-29 21:39:51 -0400

    Added two functions to find squirrel colors

--- src/SquirrelCensus.jsx
+++ src/SquirrelCensus.jsx
@@ -1,37 +1,51 @@
-import React from 'react';
-import ReactECharts from 'echarts-for-react';
+import React from "react";
+import ReactECharts from "echarts-for-react";
+import Papaparse from "papaparse";
+
+const fetchSquirrelColors = () => {
+  let surprise = Papaparse.parse("nyc_squirrels.csv", {
+    download: true,
+    complete: (results) => {
+      let colors = results.data.map((color) => {
+        return color[9];
+      });
+      let noRepeat = [...new Set(colors)];
+      console.log(noRepeat);
+    },
+  });
+  console.log("Papaparse",surprise);
+};
 
 const SquirrelCensus = () => {
-    const options = {
-        title : {
-          text: 'This is a nice bar graph',
-          textStyle: {
-            fontSize: 15
-          }
-        },
-        // grid: { top: 20, right: 8, bottom: 24, left: 36 },
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        },
-        yAxis: {
-          type: 'value',
-        },
-        series: [
-          {
-            name: 'I appear when you move the cursor',
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: 'bar',
-            smooth: true,
-          },
-        ],
-        tooltip: {
-          trigger: 'axis',
-        },
-      };
-      return <ReactECharts option={options} />;
-    };
-    
+  fetchSquirrelColors();
+  const options = {
+    title: {
+      text: "This is a nice bar graph",
+      textStyle: {
+        fontSize: 15,
+      },
+    },
+    // grid: { top: 20, right: 8, bottom: 24, left: 36 },
+    xAxis: {
+      type: "category",
+      data: [],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        name: "I appear when you move the cursor",
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: "bar",
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: "axis",
+    },
+  };
+  return <ReactECharts option={options} />;
+};
 
 export default SquirrelCensus;
-
